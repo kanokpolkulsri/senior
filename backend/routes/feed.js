92795afb839ba4be2110354dbecb1a979611176d
tonplamm <tonplam_kanokpol@hotmail.com>
2019-01-14 20:21:14 +0700

    fix: re-install backend

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond: feed');
+  res.send('respond : feed');
 });
 
 module.exports = router;
--- backend/routes/report.js
+++ backend/routes/report.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond: report');
+  res.send('respond : report');
 });
 
 module.exports = router;
