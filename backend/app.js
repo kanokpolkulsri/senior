9107fdd0a1f219da5fcc9522cfa176d3de12d2a5
tonplamm <tonplam_kanokpol@hotmail.com>
2019-01-15 14:14:53 +0700

    feat: set accessible header

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,17 @@ var review = require('./routes/review');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  next()
+})
 
 app.use('/', index);
 app.use('/register', register);
@@ -37,6 +37,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_URL: "localhost:3001",
+    API_URL: "http://localhost:3001",
 }
