834e881e66e76022573a3eb80eb44962658dbc01
baivarn-tjr <t.sukkree@hotmail.com>
2019-03-12 23:03:33 +0700

    signuplogin

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -5,20 +5,42 @@ import '../css/Register.css';
 
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        login:"Log In",
+        signup:"Sign Up",
+        loginQ: "Don't have an account?",
+        loginLink: "Create new account",
+        signupQ: "Have an account?",
+        signupLink: "Log in"
+    }
+  }
+ 
     render() {
         return (
             <div class="regis-container">
               <div class="login-block">
                 <div class="green-block">
+                  <div class="green-circle"><i class="material-icons">keyboard_arrow_right</i></div>
+                  <p class="green-block-topic">{this.props.match.path === "/Login" ? this.state.login:this.state.signup}</p>
+                  <div class="underlined"></div>
+                 
+                  <p>{this.props.match.path === "/Login" ? this.state.loginQ: this.state.signupQ} <br/>
+                   <a>{this.props.match.path === "/Login" ? this.state.loginLink: this.state.signupLink}</a> 
+                  </p>
                 </div>
+                  <Switch>
+                      <Route path="/login" component={Login}/>
+                      <Route path="/signup" component={Signup}/>
+                  </Switch>
+           
               </div>
+
                     <Link to="/signup">Sign Up</Link>
                     <Link to="/login">Log In</Link>
 
-                    <Switch>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/signup" component={Signup}/>
-                    </Switch>
+                    
             </div>
            
         )
@@ -26,13 +48,13 @@ class Register extends React.Component {
 }
 
 const Login = () => (
-    <div>
+    <div class="white-block">
       <h2>Login</h2>
     </div>
   );
   
   const Signup = () => (
-    <div>
+    <div class="white-block">
       <h2>Signup</h2>
     </div>
   );
