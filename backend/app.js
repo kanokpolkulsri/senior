04e24fe9ab3979720ac12431e7e633822b1a3843
tonplamm <tonplam_kanokpol@hotmail.com>
2019-04-05 19:19:32 +0700

    fix: API_URL frontend, now system can work

--- backend/app.js
+++ backend/app.js
@@ -30,14 +30,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
-// app.all('/*', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//   res.header('Access-Control-Allow-Credentials', true)
-//   next()
-// })
-
 app.use('/', index)
 app.use('/register', register)
 app.use('/feed', feed)
--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_URL: "ec2-18-225-33-251.us-east-2.compute.amazonaws.com:3001"
+    API_URL: "http://ec2-18-225-33-251.us-east-2.compute.amazonaws.com:3001"
 }
