2c65668f5b255eb85bd2cfc694540d8c792ebc77
baivarn-tjr <t.sukkree@hotmail.com>
2019-04-06 15:11:53 +0700

    edit config

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_URL: "http://ec2-18-225-33-251.us-east-2.compute.amazonaws.com:3001"
-    // API_URL: "mongodb://localhost:27017"
+    // API_URL: "http://ec2-18-225-33-251.us-east-2.compute.amazonaws.com:3001"
+    API_URL: "http://localhost:3001"
 }
