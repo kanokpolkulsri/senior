f362a746bc6616b78b917b244b8d1c3d0f3d102d
tonplamm <tonplam_kanokpol@hotmail.com>
2019-04-19 10:29:48 +0700

    feat: API on StudentAssignment

--- backend/routes/assignment_student.js
+++ backend/routes/assignment_student.js
@@ -19,10 +19,7 @@ router.post('/year', (req, res, next) => {
 });
 
 router.post('/student', (req, res, next) => {
-    /* get assign for that student */
-    console.log(req.body.username)
     const DB_ASSIGNMENT_STUDENT = req.app.locals.DB_ASSIGNMENT_STUDENT
-    req.body._id = mongo.ObjectID(req.body._id)
     DB_ASSIGNMENT_STUDENT.find({username: req.body.username}).toArray()
     .then(response => res.send({code: 1, data: response}))
     .catch(() => res.send({code: 0, data: ""}))
