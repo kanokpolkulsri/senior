9562b2380c8a543f6c745388b91e7dbe1a8dd3f4
tonplamm <tonplam_kanokpol@hotmail.com>
2019-04-22 21:05:01 +0700

    fix: api /form_data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,25 @@
-var express = require('express')
-var router = express.Router()
-let withAuth = require('./middleware')
+let express = require('express')
+let router = express.Router()
+let multer = require('multer')
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {cb(null, 'public/images')},
+  filename: function (req, file, cb) {cb(null, Date.now() + '-' +file.originalname )}
+})
+let upload = multer({ storage: storage }).single('file')
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
   res.send("response: index")
-});
+})
 
-module.exports = router;
+router.post('/upload', (req, res, next) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+        return res.status(500).json(err)
+    } else if (err) {
+        return res.status(500).json(err)
+    }
+    return res.status(200).send(req.file)
+  })
+})
+
+module.exports = router
